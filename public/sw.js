4bd6827bd3c6bb0adeee523ab2f4474c916e9db1
Jamerson Walderson <jamersonwalderson@gmail.com>
2026-03-22 20:56:09 -0300

    remove browser detection logic and Chrome recommendation functionality from service worker

--- public/sw.js
+++ public/sw.js
@@ -1,19 +1,3 @@
-// Detectar browser do cliente
-function detectBrowser(userAgent) {
-  const isMobile = /Android|iPhone|iPad/i.test(userAgent)
-  const isAndroid = /Android/i.test(userAgent)
-  const isAndroidChrome = /Android.*Chrome\//i.test(userAgent) && !/Edg/i.test(userAgent)
-  const isIOS = /iPhone|iPad|iPod/i.test(userAgent)
-  
-  return {
-    isMobile,
-    isAndroid,
-    isAndroidChrome,
-    isIOS,
-    shouldRecommendChrome: isAndroid && !isAndroidChrome
-  }
-}
-
 self.addEventListener('install', (event) => {
   console.log('Service Worker installing.')
   self.skipWaiting()
@@ -21,22 +5,7 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('activate', (event) => {
   console.log('Service Worker activating.')
-  
-  event.waitUntil(
-    clients.claim().then(() => {
-      // Notificar todos os clientes sobre o browser
-      return self.clients.matchAll().then(clients => {
-        clients.forEach(client => {
-          // Extrair user agent da requisição (não disponível diretamente no SW)
-          // Enviar mensagem para o cliente verificar
-          client.postMessage({
-            type: 'CHECK_BROWSER',
-            timestamp: Date.now()
-          })
-        })
-      })
-    })
-  )
+  event.waitUntil(clients.claim())
 })
 
 self.addEventListener('fetch', (event) => {
@@ -45,45 +14,4 @@ self.addEventListener('fetch', (event) => {
       return response || fetch(event.request)
     })
   )
-})
-
-// Receber mensagens dos clientes
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'BROWSER_INFO') {
-    const browserInfo = detectBrowser(event.data.userAgent)
-    
-    console.log('Browser detected:', browserInfo)
-    
-    // Se deve recomendar Chrome, enviar mensagem de volta
-    if (browserInfo.shouldRecommendChrome) {
-      event.source.postMessage({
-        type: 'RECOMMEND_CHROME',
-        browserInfo: browserInfo,
-        message: 'Para melhor experiência de instalação, abra este site no Google Chrome'
-      })
-    }
-  }
-  
-  // Responder a solicitação de instalação
-  if (event.data && event.data.type === 'REQUEST_INSTALL') {
-    const browserInfo = detectBrowser(event.data.userAgent)
-    
-    if (browserInfo.shouldRecommendChrome) {
-      // Enviar URL para abrir no Chrome
-      const url = event.data.url || self.location.origin
-      const cleanUrl = url.replace(/^https?:\/\//, '')
-      const chromeIntent = `intent://${cleanUrl}#Intent;scheme=https;package=com.android.chrome;end;`
-      
-      event.source.postMessage({
-        type: 'OPEN_IN_CHROME',
-        chromeIntent: chromeIntent,
-        fallbackUrl: `googlechrome://${cleanUrl}`
-      })
-    } else {
-      // Permitir instalação normal
-      event.source.postMessage({
-        type: 'PROCEED_INSTALL'
-      })
-    }
-  }
 })
